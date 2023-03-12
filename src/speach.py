f199a62c80e020284b53a11fd98ec6bef442d6b0
David Watson <davidpeterwatson@gmail.com>
2023-03-12 15:51:41 +1300

    Additional Commands

--- src/speach.py
+++ src/speach.py
@@ -1,22 +1,34 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
 import json
+import os
+
+exitCommand = "exit"
+shutdownCommand = "shutdown my laptop"
 
 model = Model(r"/Users/david/repos/github/DavidPeterWatson/home-automation/src/vosk/vosk-model-small-en-us-0.15")
 recognizer = KaldiRecognizer(model, 16000)
 
-mic = pyaudio.PyAudio()
-stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-stream.start_stream()
 
-done = False
+def listen():
+    mic = pyaudio.PyAudio()
+    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+    stream.start_stream()
 
-while not done:
-    data = stream.read(1024)
-    if recognizer.AcceptWaveform(data):
-        resultString = recognizer.Result()
-        result = json.loads(resultString)
-        print(result)
-        if result['text'] == "exit":
-            print("Closing")
-            done = True
+    done = False
+
+    while not done:
+        data = stream.read(1024)
+        if recognizer.AcceptWaveform(data):
+            resultString = recognizer.Result()
+            result = json.loads(resultString)
+            print(result)
+            sentence = result['text']
+            if sentence == exitCommand:
+                print("Closing")
+                done = True
+            if sentence == shutdownCommand:
+                shutdownLaptop()
+
+def shutdownLaptop():
+   os.system("shutdown /s /t 1")
